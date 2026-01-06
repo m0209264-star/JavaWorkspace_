3c62086c8cf4bb48e4533431d8d647c3a2636794
user1 <user1@DESKTOP-CJQ195S>
2026-01-06 11:12:36 +0900

    10일차

---
+++ 09_Polymorphism/src/com/kh/practice3/controller/AnimalManager.java
@@ -0,0 +1,22 @@
+package com.kh.practice3.controller;
+
+import com.kh.practice3.model.vo.Animal;
+import com.kh.practice3.model.vo.Cat;
+import com.kh.practice3.model.vo.Dog;
+
+public class AnimalManager {
+	
+	public static void main(String[] args) {
+		Animal arr [] = new Animal [5];
+		arr[0] = new Dog("꽃분이","말티즈",3);
+		arr[1] = new Dog("찐빵이","닥스훈트",5);
+		arr[2] = new Cat("네로","코숏","부천","검은색");
+		arr[3] = new Cat("코코","브숏","목동","회색");
+		arr[4] = new Dog("콩순이","푸들",5);
+		
+		for(Animal ani : arr) {
+			ani.speak();
+		}
+		
+	}
+}
